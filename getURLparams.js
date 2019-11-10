41511ddaf114e5c39466837f0ce4886d44375464
Jake Gillenwater <JakeGill70@gmail.com>
2019-11-10 18:30:56 -0500

    Fixed function name to match file name

--- getURLparams.js
+++ getURLparams.js
@@ -1,7 +1,7 @@
 // Taken from the following URL on 11/10/19
 // https://html-online.com/articles/get-url-parameters-javascript/
 
-function getUrlVars() {
+function getURLparams() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
         vars[key] = value;
