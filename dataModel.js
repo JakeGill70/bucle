9c35fb6219865f3b5f3f429c549162919fdd6dbc
Jake Gillenwater <JakeGill70@gmail.com>
2019-11-10 21:01:54 -0500

    Md5 Source Replaced
    Needed to use a different library to make the object IDs

--- dataModel.js
+++ dataModel.js
@@ -9,26 +9,26 @@ One Vote -> One User
 */
 
 function Organization(name){
-    this.name = name.toLowerCase() // Force the organization name to lower case so that is can be used in autocomplete-search.js
-}
+    this.name = name.toLowerCase(); // Force the organization name to lower case so that is can be used in autocomplete-search.js
+};
 
 function Meeting(organization, title, dateTime){
     this.organization = organization,
     this.title = title,
     this.dateTime = dateTime,
-    this.id = function(){ return (CryptoJS.MD5("meeting"+this.title+this.date+this.time));}
-}
+    this.id = function(){ return (md5("meeting"+this.title+this.date+this.time));}
+};
 
 function Agenda(meetingID){
     this.meetingID = meetingID,
-    this.id = function(){ return (CryptoJS.MD5("agenda"+this.title+this.meeting));}
-}
+    this.id = function(){ return (md5("agenda"+this.title+this.meeting));}
+};
 
 function Item(agendaID, content){
     this.agendaID = agendaID,
     this.content = content,
-    this.id = function(){ return (CryptoJS.MD5("item"+this.agendaID+this.content));}
-}
+    this.id = function(){ return (md5("item"+this.agendaID+this.content));}
+};
 
 function Motion( itemID, motionedBy, secondedBy, content, requiresVote){
     this.itemID = itemID,
@@ -36,14 +36,14 @@ function Motion( itemID, motionedBy, secondedBy, content, requiresVote){
     this.secondedBy = secondedBy, // User who seconded the motion
     this.content = content,
     this.requiresVote = requiresVote,
-    this.id = function(){ return (CryptoJS.MD5("motion"+this.itemID+this.content));}
-}
+    this.id = function(){ return (md5("motion"+this.itemID+this.content));}
+};
 
 function Vote(motionID, user, vote){
     this.motionID = motionID,
     this.user = user, // Person who cast vote
     this.vote = vote // How did they vote? [Yes | No | Present]
-}
+};
 
 function User(organization, firstName, lastName, emailAddress, password){
     this.organization = organization,
@@ -52,6 +52,6 @@ function User(organization, firstName, lastName, emailAddress, password){
     this.fullName = function(){returns (this.firstName + " " + this.lastName);},
     this.emailAddress = emailAddress,
     this.password = password,
-    this.id = function(){returns (CryptoJS.MD5(this.fullName() + this.emailAddress)); },
+    this.id = function(){returns (md5(this.fullName() + this.emailAddress)); },
     this.userRole = "member"
-}
+};
