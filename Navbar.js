5b0f0c1e9b474a66662baf4603dd921b97087796
boutonj <54907797+boutonj@users.noreply.github.com>
2019-11-13 22:19:02 -0500

    Login Modal, Navbar
    Added Navigation bar to main site - generated by navbar.js, included styles for navbar and sub links.  Includes modal popup for login functionality.  Json still needs to be set up for the login form located in the login modal

---
+++ Navbar.js
@@ -0,0 +1,5 @@
+function LoginPopUp(){
+$('#LoginModal').modal('toggle');
+}
+
+document.writeln("<nav class = 'Navbar'><a href = 'index.html' class = 'nav-link'>Bucl</a><a href = 'index.html' class = 'subnav-link'>Organizations</a><a href = 'Agendas.html' class = 'subnav-link'>Agendas</a><a href = 'Meetings.html' class = 'subnav-link'>Meetings</a><a onclick = 'LoginPopUp()' id = 'loginButton' href = '#'class = 'nav-link' style = 'float: right;'>Login</a></nav>");
